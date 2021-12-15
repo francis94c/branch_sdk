907570ab0d50b082650f08c46d441d6ded6f000b
Francis Ilechukwu <francis94c@gmail.com>
2021-12-15 22:18:09 +0100

    iOS Branch logout.

--- ios/Classes/SwiftBranchSdkPlugin.swift
+++ ios/Classes/SwiftBranchSdkPlugin.swift
@@ -1,14 +1,28 @@
 import Flutter
 import UIKit
+import Branch
 
 public class SwiftBranchSdkPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "branch_sdk", binaryMessenger: registrar.messenger())
-    let instance = SwiftBranchSdkPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "branch_sdk", binaryMessenger: registrar.messenger())
+        let instance = SwiftBranchSdkPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "init":
+            Branch.getInstance()
+        case "validateSDKIntegration":
+            Branch.getInstance().validateSDKIntegration()
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "logout":
+            Branch.getInstance().logout()
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+        
+    }
 }
