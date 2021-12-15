11b5b9354925f1bddc8853d23180bdfe3a5b63a5
Francis Ilechukwu <francis94c@gmail.com>
2021-12-15 22:07:34 +0100

    Update branch_sdk.dart

--- lib/branch_sdk.dart
+++ lib/branch_sdk.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -9,5 +8,15 @@ class BranchSdk {
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static init() {
+    _channel.invokeMethod('init');
+  }
+
+  /// Use the `validateSDKIntegration` method as a debugging aid to assure that
+  /// you've integrated the Branch SDK correctly.
+  static validateSDKIntegration() {
+    _channel.invokeMethod('validateSDKIntegration');
   }
 }
