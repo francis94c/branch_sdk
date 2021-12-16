c180b4ca7cca76ffa7f9559239399a291899aff0
Francis Ilechukwu <francis94c@gmail.com>
2021-12-16 07:29:51 +0100

    SDK functions.

--- lib/branch_sdk.dart
+++ lib/branch_sdk.dart
@@ -5,13 +5,63 @@ import 'package:flutter/services.dart';
 class BranchSdk {
   static const MethodChannel _channel = MethodChannel('branch_sdk');
 
+  /// Get platform version. Boilerplate Flutter plugin function.
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static init() {
-    _channel.invokeMethod('init');
+  /// Initailize the Branch SDK.
+  static init({bool? debug, Function? onInit}) async {
+    var params = {};
+
+    if (debug == true) {
+      params['debug'] = true;
+    }
+
+    bool result = await _channel.invokeMethod('init', params);
+
+    if (result) {
+      onInit?.call();
+    }
+  }
+
+  /// Clears user session and creates a new one. Use this when another user
+  /// wants to use the app.
+  static void logout(Function? logoutCallback) async {
+    if (logoutCallback != null) {
+      bool result = await _channel.invokeMethod('logout', [
+        {"logoutCallback": true}
+      ]);
+      if (result) {
+        logoutCallback();
+      }
+    } else {
+      await _channel.invokeMethod('logout');
+    }
+  }
+
+  static void setIdentity(String identity) async {
+    await _channel.invokeMethod('setIdentity', identity);
+  }
+
+  /// Enable Branch SDK Logging.
+  static void enableLogging() async {
+    await _channel.invokeMethod('enableLogging');
+  }
+
+  /// wrapper method to add the pre-install campaign analytics
+  static void setPreinstallCampaign(String? campaign) async {
+    await _channel.invokeMethod('setPreinstallCampaign', [
+      {"preinstallCampaign": campaign}
+    ]);
+  }
+
+  /// wrapper method to add the pre-install campaign analytics
+  static void setPreinstallPartner(String? partner) async {
+    await _channel.invokeMethod('setPreinstallPartner', [
+      {"preInstallPartner": partner}
+    ]);
   }
 
   /// Use the `validateSDKIntegration` method as a debugging aid to assure that
